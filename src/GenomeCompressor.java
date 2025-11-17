5f82ceaf55d4c0cc92ede4ee880daab518778fb4
oliverfaris <oliver.faris@menloschool.org>
2025-11-17 10:08:24 -0800

    Finished V2

--- src/GenomeCompressor.java
+++ src/GenomeCompressor.java
@@ -18,15 +18,25 @@
  *  @author Zach Blick
  */
 public class GenomeCompressor {
+    private static final int[] arr = {'A','C','T','G'};
 
     /**
      * Reads a sequence of 8-bit extended ASCII characters over the alphabet
      * { A, C, T, G } from standard input; compresses and writes the results to standard output.
      */
     public static void compress() {
-
         // TODO: complete the compress() method
-
+        String genome = BinaryStdIn.readString();
+        int num = -1;
+        for (int i =0; i < genome.length(); i++) {
+            switch (genome.charAt(i)) {
+                case 'A' -> num =0;
+                case 'C' -> num =1;
+                case 'T' -> num =2;
+                case 'G' -> num =3;
+            }
+            BinaryStdOut.write(num, 2);
+        }
         BinaryStdOut.close();
     }
 
@@ -34,9 +44,12 @@ public class GenomeCompressor {
      * Reads a binary sequence from standard input; expands and writes the results to standard output.
      */
     public static void expand() {
-
         // TODO: complete the expand() method
-
+        int nucleotide = -1;
+        while (!BinaryStdIn.isEmpty()) {
+            nucleotide = BinaryStdIn.readInt(2);
+            BinaryStdOut.write(arr[nucleotide], 8);
+        }
         BinaryStdOut.close();
     }
 
@@ -48,7 +61,6 @@ public class GenomeCompressor {
      * @param args the command-line arguments
      */
     public static void main(String[] args) {
-
         if      (args[0].equals("-")) compress();
         else if (args[0].equals("+")) expand();
         else throw new IllegalArgumentException("Illegal command line argument");
